e39e77d2d0abe5cbe1fe52e764e89ae101f6585e
root <csanad.t@gmail.com>
2023-11-05 17:25:40 +0000

    Updates

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from utils import *
 champions_df = pd.read_pickle('champions_df.pkl')
 relations_df = pd.read_pickle('relations_df.pkl')
 
-while True:
-    first_guess = str(input('Which champion was your first guess today?')).title()
-    first_guess_color_scheme = str(
-        input('What were the colors for the given guess?')
-    ).upper()
-    candidate_list = matching_champs(
-    first_guess,
-    relations_df,
-    first_guess_color_scheme
-    )
+guess_champ = str(input('Which champion was your first guess today?')).title()
+guess_color_scheme = str(
+    input('What were the colors for the given guess?')
+).upper()
+guess = Guess(guess_champ, guess_color_scheme)
+next_best_guess = guess.best_guess(relations_df)
+while len(guess)
--- utils.py
+++ utils.py
@@ -2,34 +2,38 @@ import numpy as np
 import pandas as pd
 from scipy.stats import entropy
 
-def matching_champs(
-    guessed_champ:str,
-    relations_df:pd.DataFrame,
-    color_code:str
-    ) -> list:
-    return_list = relations_df.loc[
-        (relations_df['Guessed Champion'] == guessed_champ)\
-        & (relations_df['Comparison'] == color_code)
-    ]['Candidate Champions'].to_list()
-    return return_list
+class Guess():
 
-def champ_entropy(champ:str, df:pd.DataFrame) -> float:
-    champ_probs = (
-        df.loc[df['Guessed Champion'] == champ]['Comparison']\
-    .value_counts()
-    ).to_numpy()
-    return entropy(pk=champ_probs)
+    def __init__(self, champion, color_answer):
+        self.champion = champion
+        self.color_answer = color_answer
+    
+    def matching_champs(
+        self,
+        relations_df:pd.DataFrame
+        ) -> list:
+        return_list = relations_df.loc[
+            (relations_df['Guessed Champion'] == self.champ)\
+            & (relations_df['Comparison'] == self.color_answer)
+        ]['Candidate Champions'].to_list()
+        return return_list
 
-def best_guess(
-    candidates_list:list,
-    relations_df:pd.DataFrame
-    ) -> str:
-    entropy_dict = {el:champ_entropy(el, relations_df) for el in candidates_list}
-    sorted_entropy = sorted(
-        entropy_dict.items(),
-        key=lambda item: item[1],
-        reverse=True
-    )
-    return sorted_entropy[0][0]
+    def champ_entropy(self, df:pd.DataFrame) -> float:
+        champ_probs = (
+            df.loc[df['Guessed Champion'] == self.champion]['Comparison']\
+        .value_counts()
+        ).to_numpy()
+        return entropy(pk=champ_probs)
 
-def create_relations(champions_df:pd.DataFrame) -> pd.DataFrame:
+    def best_guess(
+        self,
+        relations_df:pd.DataFrame
+        ) -> str:
+        candidates_list = self.matching_champs(relations_df=relations_df)
+        entropy_dict = {el:self.champ_entropy(el, relations_df) for el in candidates_list}
+        sorted_entropy = sorted(
+            entropy_dict.items(),
+            key=lambda item: item[1],
+            reverse=True
+        )
+        return sorted_entropy[0][0]
