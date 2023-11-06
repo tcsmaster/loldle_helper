a4686d5e6b4f527d3fa14d4b7ccbd635fcfdef5d
root <csanad.t@gmail.com>
2023-11-06 17:12:04 +0000

    Updates

--- utils.py
+++ utils.py
@@ -24,27 +24,30 @@ class Guess():
     
     def matching_champs(
         self,
-        relations_df:pd.DataFrame
+        df:pd.DataFrame
         ) -> list:
-        return_list = relations_df.loc[
-            (relations_df['Guessed Champion'] == self.champ)\
-            & (relations_df['Comparison'] == self.color_answer)
-        ]['Candidate Champions'].to_list()
+        return_list = df.loc[
+            (df['Guessed_Champion'] == self.champ)\
+            & (df['Comparison'] == self.color_answer)
+        ]['Actual_Champions'].to_list()
         return return_list
 
-    def champ_entropy(self, df:pd.DataFrame) -> float:
+    def champ_entropy(
+        self,
+        df:pd.DataFrame
+        ) -> float:
         champ_probs = (
-            df.loc[df['Guessed Champion'] == self.champion]['Comparison']\
+            df.loc[df['Guessed_Champion'] == self.champion]['Comparison']\
         .value_counts()
         ).to_numpy()
         return entropy(pk=champ_probs)
 
     def best_guess(
         self,
-        relations_df:pd.DataFrame
+        df:pd.DataFrame
         ) -> str:
-        candidates_list = self.matching_champs(relations_df=relations_df)
-        entropy_dict = {el:self.champ_entropy(el, relations_df) for el in candidates_list}
+        candidates_list = self.matching_champs(df=df)
+        entropy_dict = {el:self.champ_entropy(el, df) for el in candidates_list}
         sorted_entropy = sorted(
             entropy_dict.items(),
             key=lambda item: item[1],
@@ -56,11 +59,11 @@ def get_comparison_from_champs(df):
     a = [Champion(**df.iloc[i, :]) for i in range(df.shape[0])]
     combi = combinations(a, 2)
 
-    relations_df = pd.DataFrame(columns = ['Guessed_Champion', 'Actual_Champion', 'Comparison'])
+    df = pd.DataFrame(columns = ['Guessed_Champion', 'Actual_Champion', 'Comparison'])
     for el in combi:
-        relations_df = pd.concat(
+        df = pd.concat(
             [
-                relations_df,
+                df,
                 pd.DataFrame(
                     [
                         {
@@ -72,7 +75,7 @@ def get_comparison_from_champs(df):
                 )
             ]
         )
-    return relations_df
+    return df
 
 def get_result_of_comparison(guessed_champion:Champion, candidate_champion:Champion) -> str:
     result = ''
