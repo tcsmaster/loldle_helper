a6bf8a7fe41791165e41a662131e2b7acad11bfc
root <csanad.t@gmail.com>
2023-11-06 17:05:31 +0000

    Updates

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
 import numpy as np
 import pandas as pd
+
+from itertools import combinations
+from pydantic import BaseModel
+from typing import List
 from scipy.stats import entropy
+
+class Champion(BaseModel):
+        Name:str
+        Gender: str
+        Position: str|List[str]
+        Species: str|List[str]
+        Resource: str
+        Range_type: str|List[str]
+        Region: str|List[str]
+        Release_year:int
 
 class Guess():
 
@@ -37,3 +51,60 @@ class Guess():
             reverse=True
         )
         return sorted_entropy[0][0]
+
+def get_comparison_from_champs(df):
+    a = [Champion(**df.iloc[i, :]) for i in range(df.shape[0])]
+    combi = combinations(a, 2)
+
+    relations_df = pd.DataFrame(columns = ['Guessed_Champion', 'Actual_Champion', 'Comparison'])
+    for el in combi:
+        relations_df = pd.concat(
+            [
+                relations_df,
+                pd.DataFrame(
+                    [
+                        {
+                            'Guessed_Champion':el[0].Name,
+                            'Actual_Champion':el[1].Name,
+                            'Comparison':get_result_of_comparison(*el)
+                        }
+                    ]
+                )
+            ]
+        )
+    return relations_df
+
+def get_result_of_comparison(guessed_champion:Champion, candidate_champion:Champion) -> str:
+    result = ''
+    for (attr, val1), (_, val2) in zip(vars(guessed_champion).items(), vars(candidate_champion).items()):
+        if attr == 'Name':
+            pass
+        elif val1 == val2:
+            result += 'G'
+        elif attr == 'Release_year':
+            res = 'G' if val1 == val2 else 'R'
+            result += res
+        else:
+            both_string = 0
+            if isinstance(val1, str):
+                val1 = set([val1])
+                both_string += 1
+            
+            if isinstance(val2, str):
+                val2= set([val2])
+                both_string += 1
+
+            set1 = set(val1)
+            set2 = set(val2)
+            overlap = bool(set1.intersection(set2))
+            if both_string < 2:
+                if overlap:
+                    result += 'O'
+                else:
+                    result += 'R'
+            else:
+                if overlap:
+                    result += 'G'
+                else:
+                    result += 'R'
+    return result
